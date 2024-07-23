e87fdcc1c16cb112eb86bb7e26f4ec4d94a90756
Sebastian Eichhammer <sebastian.eichhammer@icloud.com>
2024-07-23 10:36:40 +0200

    github pages for vite react

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/my-react-app/", //github pages
   server: {
     port: 3000,
     strictPort: true,
